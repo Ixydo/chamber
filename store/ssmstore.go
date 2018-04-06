a10e56f47d93a88c1d3d673e23f02946fbe467d8
Doug Neal <doug@neal.me.uk>
2018-04-06 19:04:00 +0100

    Allow dots in key names

--- store/ssmstore.go
+++ store/ssmstore.go
@@ -22,7 +22,7 @@ const (
 
 // validPathKeyFormat is the format that is expected for key names inside parameter store
 // when using paths
-var validPathKeyFormat = regexp.MustCompile(`^\/[A-Za-z0-9-_/]+$`)
+var validPathKeyFormat = regexp.MustCompile(`^\/[A-Za-z0-9-_/.]+$`)
 
 // validKeyFormat is the format that is expected for key names inside parameter store when
 // not using paths
